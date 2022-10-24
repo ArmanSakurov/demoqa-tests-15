0564e355198e3657bd21382fa2972ec475e695f3
Aleksandr Rogachev <99810791+Aleksandr-272014-Rogachev@users.noreply.github.com>
2022-10-24 12:57:21 +0300

    first commit

--- src/test/java/com/demoqa/tests/SearchTests.java
+++ src/test/java/com/demoqa/tests/SearchTests.java
@@ -2,6 +2,8 @@ package com.demoqa.tests;
 
 import org.junit.jupiter.api.Test;
 
+import javax.swing.*;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
@@ -13,4 +15,20 @@ public class SearchTests {
         $("[name=q]").setValue("selenide").pressEnter();
         $("[id=search]").shouldHave(text("Selenide"));
     }
+
+    @Test
+    void textBoxTest() {
+        open("https://demoqa.com/text-box");
+        $("[id=userName]").setValue("Sasha").pressEnter();
+        $("[id=userEmail]").setValue("sasha@mail.ru").pressEnter();
+        $("[id=currentAddress]").setValue("Omsk").pressEnter();
+        $("[id=permanentAddress]").setValue("street").pressEnter();
+        $("[id=submit]").click();
+
+        $("[id=output]").shouldHave(text("Sasha"));
+        $("[id=output]").shouldHave(text("sasha@mail.ru"));
+        $("[id=output]").shouldHave(text("Omsk"));
+        $("[id=output]").shouldHave(text("street"));
+    }
+
 }
