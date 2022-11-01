cf9ec639092565e4bacfe3696c1505153348aae1
ArmanSakurov <a.sakurov@skyeng.ru>
2022-11-01 18:31:35 +0500

    [sasha] added beforeAll annotation

--- src/test/java/com/demoqa/tests/SearchTests.java
+++ src/test/java/com/demoqa/tests/SearchTests.java
@@ -1,5 +1,7 @@
 package com.demoqa.tests;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
@@ -7,6 +9,13 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests {
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+
     @Test
     void successfulSearchTest() {
         open("https://www.google.com/");
